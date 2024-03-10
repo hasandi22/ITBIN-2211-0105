36c4e1f2ef212b5f449a0227f27d10ad8d761320
hasandi22 <himasarieuyangoda@gmail.com>
2024-03-10 17:01:51 +0530

    commiting the edited file

--- Inky.java
+++ Inky.java
@@ -1,1 +1,24 @@
+//ITBIN-2211-0105
 
+public class Inky{
+
+public Inky(String pinky){
+System.out.println(pinky);
+}
+public Inky(String pinky, String blinky, String rinky){
+System.out.println(blinky +"" +pinky);
+System.out.println(pinky +""  +rinky);
+System.out.println(blinky +"" +pinky +"" +rinky);
+}
+
+//main method
+public static void main(String args[]){
+String pinky = "Donky";
+String blinky = "Pinky";
+String rinky = "Monkey";
+
+//creating objects(instances) with parameters
+Inky ponky=new Inky(pinky);
+Inky ponky2=new Inky(pinky,blinky,rinky);
+}
+}
